9cd06676d958b090f8dc4feeaeeca995a35ca239
omar zarate <alberich_sid@hotmail.com>
2018-01-10 17:23:23 -0600

    se cambio el puertode ejecucion va ser 3333 y ademas se agrego el apartado de mantenimiento

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,80 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+<html lang="es">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<head>
+  <meta charset="utf-8">
+  <title>Mantenimiento</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+  <meta property="og:title" content="">
+  <meta property="og:image" content="">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="">
+  <meta name="twitter:title" content="">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:image" content="">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+  <!-- Favicon -->
+  <link href="img/favicon.ico" rel="icon">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700|Roboto:400,900" rel="stylesheet">
+  <!-- Bootstrap CSS File -->
+  <link href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <!-- Libraries CSS Files -->
+  <link href="{{ asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+  <!-- Main Stylesheet File -->
+  <link href="{{ asset('lib/style.css') }}" rel="stylesheet">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Vueva Pronto.</div>
-            </div>
+</head>
+
+<body>
+  <section class="hero">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-md-12">
+          <a class="hero-brand" title="Home">COMERCIALIZACION</a>
         </div>
-    </body>
+      </div>
+
+      <div class="col-md-12">
+        <h1>
+            ESTAMOS EN MANTENIEMENTO
+          </h1>
+
+        <p class="tagline">
+          Pronto Volveremos en linea.
+        </p>
+
+      </div>
+    </div>
+
+  </section>
+
+  <!-- Required JavaScript Libraries -->
+  <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('lib/jquery/jquery-migrate.min.js') }}"></script>
+  <script src="{{ asset('lib/superfish/hoverIntent.js') }}"></script>
+  <script src="{{ asset('lib/superfish/superfish.min.js') }}"></script>
+  <script src="{{ asset('lib/tether/js/tether.min.js') }}"></script>
+  <script src="{{ asset('lib/stellar/stellar.min.js') }}"></script>
+  <script src="{{ asset('lib/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('lib/counterup/counterup.min.js') }}"></script>
+  <script src="{{ asset('lib/waypoints/waypoints.min.js') }}"></script>
+  <script src="{{ asset('lib/easing/easing.js') }}"></script>
+  <script src="{{ asset('lib/stickyjs/sticky.js') }}"></script>
+  <script src="{{ asset('lib/parallax/parallax.js') }}"></script>
+  <script src="{{ asset('lib/lockfixed/lockfixed.min.js') }}"></script>
+  <!-- Template Specisifc Custom Javascript File -->
+  <script src="js/custom.js"></script>
+
+  <script src="contactform/contactform.js"></script>
+
+</body>
 </html>
